1e970a89b612d8b0b52c5821b47dc8d428ef6c1b
iborhim <iborhim@gamail.com>
2024-05-10 13:44:25 +0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <Wrapper>
       <div className="nav-center">
-        <span className="logo">MixMaster</span>
+        <span className="logo">The CocktailDB</span>
         <div className="nav-links">
           <NavLink to="/" className="nav-link">
             Home
